6c07824f43050b6c913167eb1fced33d530c9a76
Yousif Elia <yousif.eliaa@gmail.com>
2026-03-03 14:45:01 -0500

    Update GamePanel.java

--- Lab6/GamePanel.java
+++ Lab6/GamePanel.java
@@ -1,9 +1,11 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 
 public class GamePanel extends JPanel {
 
@@ -14,17 +16,20 @@ public class GamePanel extends JPanel {
     private int y = 200;
 
     private int selectedCustomization = -1;
+    
+    private Set<Integer> pressedKeys = new HashSet<>();
 
     public GamePanel() {
         try {
-            character = ImageIO.read(new File("character.png"));
+            character = ImageIO.read(new File("player.png"));
 
-            customizations = new BufferedImage[5];
-            customizations[0] = ImageIO.read(new File("hat.png"));
-            customizations[1] = ImageIO.read(new File("glasses.png"));
-            customizations[2] = ImageIO.read(new File("shirt.png"));
-            customizations[3] = ImageIO.read(new File("sword.png"));
-            customizations[4] = ImageIO.read(new File("cape.png"));
+            customizations = new BufferedImage[6];
+            customizations[0] = ImageIO.read(new File("player.png"));
+            customizations[1] = ImageIO.read(new File("hat.png"));
+            customizations[2] = ImageIO.read(new File("glasses.png"));
+            customizations[3] = ImageIO.read(new File("armor.png"));
+            customizations[4] = ImageIO.read(new File("sword.png"));
+            customizations[5] = ImageIO.read(new File("cape.png"));
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -35,17 +40,27 @@ public class GamePanel extends JPanel {
 
         addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent e) {
+                pressedKeys.add(e.getKeyCode());
+            }
 
-                int key = e.getKeyCode();
-
-                if (key == java.awt.event.KeyEvent.VK_W) y -= 10;
-                if (key == java.awt.event.KeyEvent.VK_S) y += 10;
-                if (key == java.awt.event.KeyEvent.VK_A) x -= 10;
-                if (key == java.awt.event.KeyEvent.VK_D) x += 10;
-
-                repaint();
+            public void keyReleased(java.awt.event.KeyEvent e) {
+                pressedKeys.remove(e.getKeyCode());
             }
         });
+        
+        Timer gameLoop = new Timer(50, e -> update());
+        gameLoop.start();
+    }
+    
+    private void update() {
+        int speed = 5;
+        
+        if (pressedKeys.contains(java.awt.event.KeyEvent.VK_W)) y -= speed;
+        if (pressedKeys.contains(java.awt.event.KeyEvent.VK_S)) y += speed;
+        if (pressedKeys.contains(java.awt.event.KeyEvent.VK_A)) x -= speed;
+        if (pressedKeys.contains(java.awt.event.KeyEvent.VK_D)) x += speed;
+        
+        repaint();
     }
 
     public void setCustomization(int index) {
@@ -56,10 +71,9 @@ public class GamePanel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        if (character != null)
-            g.drawImage(character, x, y, null);
-
         if (selectedCustomization >= 0)
             g.drawImage(customizations[selectedCustomization], x, y, null);
+        else if (character != null)
+            g.drawImage(character, x, y, null);
     }
 }
