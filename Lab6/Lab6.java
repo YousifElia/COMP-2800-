9734671dd806018609cf4372845edb7f59d4c1aa
Yousif Elia <yousif.eliaa@gmail.com>
2026-03-24 16:59:09 -0400

    Update Lab6.java

--- Lab6/Lab6.java
+++ Lab6/Lab6.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 public class Lab6 extends JFrame {
 
     public Lab6() {
+
         setTitle("Character Customization");
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -14,14 +15,14 @@ public class Lab6 extends JFrame {
 
         add(gamePanel, BorderLayout.CENTER);
         add(controlPanel, BorderLayout.SOUTH);
+
         setVisible(true);
 
-        // request focus AFTER frame is visible
+        // Ensure focus for WASD
         gamePanel.requestFocusInWindow();
     }
 
     public static void main(String[] args) {
-        new Lab6();
+        SwingUtilities.invokeLater(() -> new Lab6());
     }
 }
-
